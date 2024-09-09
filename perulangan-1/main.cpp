d3edc26de8afc4c7d451e69601e60bd1c1634ae0
Gumilangtirtha <tirthaperdana@gmail.com>
2024-09-09 07:53:13 +0700

    perulangan CodeBlock

---
+++ perulangan-1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Perulangan For!" << endl<< endl;
+    for(int i=1; i<=3; i++)
+
+   {
+
+    cout<< "saya ";
+    cout<<"Belajar Program";
+    cout<<i<<endl;
+
+   }
+
+    return 0;
+}
